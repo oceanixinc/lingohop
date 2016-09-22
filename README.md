4456b8c6bba27fbf214d984ec9795b7eccb83751
Mrinal Mech <mrinalmech@gmail.com>
2016-09-22 10:45:08 +0530

    Separation of client/server

--- README.md
+++ README.md
@@ -1,22 +1,20 @@
 ### Welcome to Lingohop
 
+#### Server configuration
 
-#### Basic configuration
-
-1. Clone the project at your folder
+1. Clone the project and go to the 'server' folder
 2. Activate your virtual environment
-3. Go inside cloned folder where you can find 'requirements.txt'
+3. Go inside 'server' folder where you can find 'requirements.txt'
 4. Run - `pip install -r requirements.txt`
 5. Create database with command - `createdb lingohop`
 6. Run - `python manage.py migrate`
-7. Open another tab in your terminal and go inside 'material' folder
+7. Run - `python manage.py runserver`
 
-	7.a run `npm install`
+#### Client configuration
 
-	7.b run `sudo npm start` 
-
-8. Run - `python manage.py runserver` in your previous tab
-
+1. Go to 'client' folder
+2. run `npm install`
+3. run `npm start`
 
 #### Database configuration
 
---
+++ client/src/app.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import {render} from 'react-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+// import Main from './Main'; // Our custom react component
+// import Main from './TestComponentTheme'; // Our custom react component
+import Main from './Theme'; // Our custom react component
+import AppBarExampleIconMenu from './header'; // Our custom react component
+// import ToolbarExamplesSimple from './toolbar'; // Our custom react component
+import FrontVideoComponent from './Frontheader'; // Our custom react component
+import DiscoverPanel from './DiscoverPanel'; // Our custom react component
+import ExperiencePanel from './ExperiencePanel'; // Our custom react component
+import FooterPanel from './FooterPanel'; // Our custom react component
+import App from './Main'; // Our custom react component
+import Home from './Home'; // Our custom react component
+import SignUp from './SignUp'; // Our custom react component
+import NotFound from './NotFound'; // Our custom react component
+
+import Paper from 'material-ui/Paper';
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+
+import { browserHistory, Router, Route,  IndexRoute, IndexLink, Link } from 'react-router'
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+
+// Render the main app react component into the app div.
+// For more details see: https://facebook.github.io/react/docs/top-level-api.html#react.render
+
+// const AppHeader = () => (
+//   <MuiThemeProvider>
+//     <CardExampleWithAvatar />
+//   </MuiThemeProvider>
+// );
+
+
+const ExperienceLabel = () => (
+ <MuiThemeProvider>
+  <div style={{textAlign: 'center', marginTop: 30}}>
+
+      <CardTitle
+
+      title="An experience just for you" subtitle="Learn a language based on your unique needs." />
+
+  </div>
+  </MuiThemeProvider>
+);
+
+
+// const AppDiscover = () => (
+//   <MuiThemeProvider>
+//     <DiscoverPanel />
+//   </MuiThemeProvider>
+// );
+
+
+
+
+// render(<SignUp />, document.getElementById('app'));
+// render(<FrontVideoComponent />, document.getElementById('header'));
+// render(<DiscoverPanel />, document.getElementById('discover'));
+// render(<ExperienceLabel />, document.getElementById('experienceLabel'));
+// render(<ExperiencePanel />, document.getElementById('experiencePanel'));
+// render(<FooterPanel />, document.getElementById('footer'));
+
+render((
+   <Router history={browserHistory}>
+    <Route path="/" component={App}>
+     <IndexRoute component={Home}/>
+      <Route path="signup" component={SignUp} />
+      <Route path='*' component={NotFound} />
+    </Route>
+
+  </Router>), document.getElementById('app'));
