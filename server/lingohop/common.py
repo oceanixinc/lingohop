9b3376290889c454ca20d9a2d7abb64ed4388f78
Sijan <sijanbhandari@oceanixinc.com>
2016-09-30 17:56:28 +0545

    change in path 1

--- server/lingohop/common.py
+++ server/lingohop/common.py
@@ -167,6 +167,8 @@ USE_L10N = True
 USE_TZ = True
 
 
+STATIC_ROOT = str(ROOT_DIR("../../../../"+'static'))
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
--- server/lingohop/production.py
+++ server/lingohop/production.py
@@ -8,10 +8,6 @@ from os.path import join, dirname
 ROOT_DIR = environ.Path(__file__) - 2  # (/a/b/myfile.py - 3 = /)
 BASE_DIR = dirname(dirname(__file__))
 
-# STATIC_ROOT = str("../../../../"+ROOT_DIR('static'))
-STATIC_ROOT = join(os.path.dirname(BASE_DIR), '../../../../' 'static')
-
-
 
 # SECRET CONFIGURATION
 # ------------------------------------------------------------------------------
