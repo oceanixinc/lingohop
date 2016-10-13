0a9d437ef3b7c44ff05fa55645e7a37f9f0f3a3f
Tom Mugo <tommugo@oceanixinc.com>
2016-10-13 18:29:48 +0300

    Reload servers

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -7,4 +7,9 @@ python manage.py migrate
 
 python manage.py collectstatic --noinput
 
+sudo supervisorctl reload
+
+sudo service nginx restart
+
 echo "The AfterInstall deployment lifecycle event successfully completed." > after-install.txt
+
